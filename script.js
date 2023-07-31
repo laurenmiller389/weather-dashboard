669d5291d465ec7732770d4392668889ad699492
Lauren Miller <lmiller90@fordham.edu>
2023-07-31 17:25:08 -0400

    formattted day

---
+++ script.js
@@ -0,0 +1,7 @@
+var searchButtonElement = document.getElementById("search-button");
+var searchCityElement = document.querySelector("#city-search");
+
+var latitude;
+var longitude;
+
+var today = dayjs().format("ddd MMM D, YYYY")
