16bd67fa0ba88176a973e7a3f27d2174861821df
Lauren Miller <lmiller90@fordham.edu>
2023-07-31 17:45:40 -0400

    completed api fetch requests

--- script.js
+++ script.js
@@ -7,5 +7,24 @@ var longitude;
 var today = dayjs().format("ddd MMM D, YYYY")
 
 function getWeatherApi () {
-    var reqUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCityElement.value + "&appid=" + apiKey + "&units=imperial"
+    var reqURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCityElement.value + "&appid=" + apiKey + "&units=imperial"
+    
+    fetch(reqURL).then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        document.getElementById("city-name").textContent = data.name + " (" + today + ") ";
+        document.getElementById("city-icon").src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        document.getElementById("city-temp").textContent = "Temp: " + data.main.temp + " F\xB0";
+        document.getElementById("city-wind").textContent = "Wind: " + data.wind.speed + " MPH";
+        document.getElementById("city-humid").textContent = "Humid: " + data.main.humidity + "%";
+        longitude = data.coord.longitude;
+        latitude = data.coord.latitude;
+
+        var reqURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
+    
+        fetch(reqURL).then(function (res) {
+            return res.json();
+        })
+    })
 }
